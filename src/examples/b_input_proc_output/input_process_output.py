d4db393410c36a113fc70dba315da19f2dafc2a4
YUQING-YANG2024 <yuqing.yang@g.austincc.edu>
2024-09-05 21:21:47 +0000

    input_process_output

--- src/examples/b_input_proc_output/input_process_output.py
+++ src/examples/b_input_proc_output/input_process_output.py
@@ -5,3 +5,8 @@ def display_output():
 def test_config():
     return True
 
+def float_division(value1, value2):
+    return value1 / value2 
+
+def integer_division(value1, value2):
+    return value1 // value2 
