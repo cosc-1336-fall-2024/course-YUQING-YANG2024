8abc02b1e905492cae18cd28071397d122190c5c
YUQING-YANG2024 <yuqing.yang@g.austincc.edu>
2024-09-05 23:09:12 +0000

    HW1 multiply_numbers Part2

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,1 +1,13 @@
 #
+
+import output
+
+def main():
+    value1 = int(input("ENTER VALUE 1: "))
+    value2 = int(input("ENTER VALUE 2: "))
+
+    result = output.multiply_numbers(value1, value2)
+
+    print("RESULT: ", result)
+
+main()
